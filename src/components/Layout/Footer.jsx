343245a2d6dd6265a5398b934ce75b8ded861273
sheraz4196 <sheraztoor321@gmail.com>
2024-06-10 20:17:35 +0500

    feat: border for footer visualization

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 export default function Footer({ commonData }) {
   return (
-    <footer class="bg-white dark:bg-gray-700">
+    <footer class="bg-white dark:bg-gray-700 border-t border-primary">
       <div class="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav
           class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
