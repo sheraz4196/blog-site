0bebdf63a826dea3224e4101fe86c826148bb0ea
sheraz4196 <sheraztoor321@gmail.com>
2024-06-10 17:35:10 +0500

    fix: padding on small screens

--- src/components/Home/BlogsList.jsx
+++ src/components/Home/BlogsList.jsx
@@ -3,7 +3,7 @@ import BlogCard from "../Blog/BlogCard";
 
 export default function BlogsList({ blogs, currentPage }) {
   return (
-    <section className="max-w-7xl py-24 sm:py-32 flex flex-wrap mx-auto">
+    <section className="max-w-7xl py-24 sm:py-32 flex flex-wrap mx-auto px-6 lg:px-0">
       {blogs?.map((blog, index) => (
         <BlogCard
           data={blog}
