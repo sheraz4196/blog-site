76b713820245fbeea6acd303ae8f34bf7784eb39
sheraz4196 <sheraztoor321@gmail.com>
2024-06-10 19:54:08 +0500

    fix: authors page UI

--- src/pages/author/[slug].js
+++ src/pages/author/[slug].js
@@ -12,19 +12,19 @@ export default function Slug({ authorData, blogs }) {
       <main className="flex items-center justify-center mt-32">
         <div className="max-container flex items-center justify-center">
           <div className="mx-auto max-w-4xl">
-            <section className="py-6 border-b lg:pt-12 border-gray-100">
+            <section className="py-6 border-b lg:pt-12 border-gray-100 flex flex-col gap-6">
+              {authorData?.image?.fields?.file?.url && (
+                <div>
+                  <Image
+                    src={"https:" + authorData?.image?.fields?.file?.url}
+                    alt={authorData?.image?.fields?.title || "author"}
+                    width={110}
+                    height={110}
+                    className="rounded-full mx-auto md:mx-0"
+                  />
+                </div>
+              )}
               <div className="flex flex-col md:flex-row md:items-center gap-4 md:gap-8">
-                {authorData?.image?.fields?.file?.url && (
-                  <div>
-                    <Image
-                      src={"https:" + authorData?.image?.fields?.file?.url}
-                      alt={authorData?.image?.fields?.title || "author"}
-                      width={110}
-                      height={110}
-                      className="rounded-full mx-auto md:mx-0"
-                    />
-                  </div>
-                )}
                 <div>
                   <h1 className="text-muted-2 dark:text-white text-4xl md:text-6xl leading-tight font-semibold text-center md:text-left">
                     {authorData?.name}
