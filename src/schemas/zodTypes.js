3e3e226d2888986d44676ca12fb1501be7d12619
sheraz4196 <sheraztoor321@gmail.com>
2024-06-13 15:23:42 +0500

    feat: zod schemas for validation

---
+++ src/schemas/zodTypes.js
@@ -0,0 +1,34 @@
+import { z } from "zod";
+export const AuthorSchema = z.object({
+  sys: z.object({
+    id: z.string(),
+  }),
+  fields: z.object({
+    name: z.string(),
+    bio: z.string().optional(),
+  }),
+});
+
+export const TagSchema = z.object({
+  sys: z.object({
+    id: z.string(),
+  }),
+  fields: z.object({
+    name: z.string(),
+  }),
+});
+
+export const BlogPostSchema = z.object({
+  sys: z.object({
+    id: z.string(),
+    createdAt: z.string(),
+    updatedAt: z.string(),
+  }),
+  fields: z.object({
+    title: z.string(),
+    slug: z.string(),
+    body: z.string().optional(),
+    tags: z.array(TagSchema).optional(),
+    author: AuthorSchema.optional(),
+  }),
+});
