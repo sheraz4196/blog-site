7a20c6894c70340386d0d14e82fede31ebca68ba
sheraz4196 <sheraztoor321@gmail.com>
2024-06-14 14:45:20 +0500

    clean code

--- src/components/Blog/SimilarBlogs.jsx
+++ src/components/Blog/SimilarBlogs.jsx
@@ -3,13 +3,11 @@ import BlogCard from "./BlogCard";
 
 export default function SimilarBlogs({ similarBlogData }) {
   return (
-    <div className="flex items-center justify-center mb-9">
-      <div className="max-container">
-        <div className="mx-auto mt-16 grid grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 max-w-7xl">
-          {similarBlogData?.map((blog, index) => (
-            <BlogCard data={blog} key={index} />
-          ))}
-        </div>
+    <div className="max-container mx-auto mb-9">
+      <div className="mx-auto mt-16 grid grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 max-w-7xl">
+        {similarBlogData?.map((blog, index) => (
+          <BlogCard data={blog} key={index} />
+        ))}
       </div>
     </div>
   );
